5175c4a00f60b4e5cc07b27c8fa926f335e82099
Kondrias <danpestolesi@gmail.com>
2020-03-14 18:25:28 -0700

    Update Playfair.cpp

--- Playfair.cpp
+++ Playfair.cpp
@@ -24,7 +24,14 @@ bool Playfair::setKey(const string& key) // also creates the matrix used
 			}
 		}
 	}
-	_key.append(alphabet)
+	_key.append(alphabet);
+	int index = 0;
+	for(int row = 0; row < 5; row++){
+		for(int column = 0; column < 5; column++){
+			Keymatrix[row][column] = _key[index];
+			index++;
+		}
+	}
 	return true;
 }
 
